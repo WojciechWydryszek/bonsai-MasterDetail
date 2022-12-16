e735f356570e64ab34b8b6fd53fdda8a2de4d29c
WojciechWydryszek <wojciech.wydryszek@uvic.cat>
2022-12-16 17:54:31 +0100

    create qr generator

--- app/src/main/java/com/teknos/m8uf2/wwydryszek/screen/QrScreen.java
+++ app/src/main/java/com/teknos/m8uf2/wwydryszek/screen/QrScreen.java
@@ -1,18 +1,65 @@
 package com.teknos.m8uf2.wwydryszek.screen;
 
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.zxing.WriterException;
 import com.teknos.m8uf2.wwydryszek.R;
 import com.teknos.m8uf2.wwydryszek.singletone.Singletone;
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 public class QrScreen extends AppCompatActivity {
+
+    private EditText editText;
+    private ImageView imageView;
+
+    Bitmap bitmap;
+    QRGEncoder qrgEncoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Singletone.getInstance().getTheme();
         setContentView(R.layout.qr_screen);
+
+        editText = findViewById(R.id.editTextTextPersonName);
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public void generateQr(View view){
+        if (TextUtils.isEmpty(editText.getText().toString())) {
+
+        } else {
+            WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+            Display display = manager.getDefaultDisplay();
+
+            Point point = new Point();
+            display.getSize(point);
+
+            int width = point.x;
+            int height = point.y;
+
+            int dimen = width < height ? width : height;
+            dimen = dimen * 3 / 4;
+
+            qrgEncoder = new QRGEncoder(editText.getText().toString(), null, QRGContents.Type.TEXT, dimen);
+            try {
+                bitmap = qrgEncoder.encodeAsBitmap();
+                imageView.setImageBitmap(bitmap);
+            } catch (WriterException e) {
+                Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     // Function for change screem using toolBar
